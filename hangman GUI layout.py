3924c467d697f7679b86c87353cd73b7243e8125
Cataluna <catalunalilith2680@gmail.com>
2021-09-19 17:32:31 -0400

    fixed sizing issue with hangman image. TODO: adjust ratios of widgets and frames throughout

--- hangman GUI layout.py
+++ hangman GUI layout.py
@@ -10,25 +10,33 @@ from PIL import ImageTk, Image
 
 window = tk.Tk()
 window.title("hangman layout")
+window.columnconfigure(0, weight=1, minsize=75)
+window.columnconfigure(1, weight=1, minsize=75)
+window.rowconfigure(0, weight=1, minsize=50)
 
 # TODO add icon image
 
 # create widgets
 frame_left = tk.Frame(master=window)
-hangman_images = ImageTk.PhotoImage(Image.open("hangman pics.jpg"))
-hangman_img_label = tk.Label(master=frame_left, image=hangman_images)
+hangman_images = Image.open("hangman6Guesses.jpg")
+hangman_images = hangman_images.resize((600, 800), Image.NEAREST)
+hangman_images = ImageTk.PhotoImage(hangman_images)
+hangman_img_label = tk.Label(master=frame_left,
+                             image=hangman_images,
+                             width=500,
+                             height=800,)
 word_label = tk.Label(master=frame_left)
 guesses_label = tk.Label(master=frame_left, text="you have 6 guesses left")
 
 frame_right = tk.Frame(master=window)
 welcome_label = tk.Label(master=frame_right, text="Welcome to Hangman!")
-dialogue_label = tk.Label(master=frame_right, text ="this is where dialogue goes")
+dialogue_label = tk.Label(master=frame_right, text="this is where dialogue goes")
 avaialbe_letters_label = tk.Label(master=frame_right, text="available letters")
 letters_frame = tk.Frame(master=frame_right)
 
 # grid widgets
 frame_left.grid(row=0, column=0)
-hangman_img_label.grid(row=0, column=0)
+hangman_img_label.grid(row=0, column=0)  # sticky="nsew"
 word_label.grid(row=1, column=0)
 guesses_label.grid(row=2, column=0)
 
